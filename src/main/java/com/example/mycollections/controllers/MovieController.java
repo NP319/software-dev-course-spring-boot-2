85d9d87a8c1495b4a5350008a1cbc21eb6524932
Nikita Paul <nikitapaul229@gmail.com>
2026-06-24 16:36:41 -0500

    Practice 2

---
+++ src/main/java/com/example/mycollections/controllers/MovieController.java
@@ -0,0 +1,45 @@
+package com.example.mycollections.controllers;
+
+import com.example.mycollections.models.Movie;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    private final List<Movie> movies = new ArrayList<>() {{
+        add(new Movie("Titanic", "James Cameron", 1997, 195));
+        add(new Movie("Avatar", "James Cameron", 2009, 162));
+        add(new Movie("Inception", "Christopher Nolan", 2010, 148));
+    }};
+
+    @GetMapping("/json")
+    public List<Movie> getMoviesJson() {
+        return movies;
+    }
+
+    @GetMapping("/html")
+    public String getMoviesHtml() {
+        String movieList = "<ul>";
+        for (Movie movie : movies) {
+            movieList += "<li>" + movie + "</li>";
+        }
+        movieList += "</ul>";
+
+        return """
+                <html>
+                    <body>
+                        <h1>Movies</h1>
+                        <ul>
+                """ +
+                movieList +
+                """
+                        </ul>
+                    </body>
+                """;
+    }
+
+}
